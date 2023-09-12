32a7991e9c24993236009948e04de9be6560f51c
MulkiHafizh <ukiyo142@gmail.com>
2023-09-12 10:57:42 +0700

    Added Initial Route

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +13,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      initialRoute: '/',
+      initialRoute: 'splash',
+      routes: {
+        'splash': (context) => const SplashScreen(),
+      },
     );
   }
 }
