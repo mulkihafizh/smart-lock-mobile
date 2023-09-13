626a1d200974c34054a3d9c26db23a39fb01cb47
MulkiHafizh <ukiyo142@gmail.com>
2023-09-13 09:22:24 +0700

    Splash

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -10,10 +10,71 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Splash Screen'),
+    return Scaffold(
+        body: Container(
+      alignment: Alignment.center,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+          colors: [
+            Color(0xFF2447A9),
+            Color(0xFF67AAF3),
+          ],
+          stops: [0.01, 1.0],
+          transform: GradientRotation(168 * 3.14159265 / 180),
+        ),
       ),
-    );
+      child: Center(
+          child: Padding(
+              padding: const EdgeInsets.only(right: 40, left: 40),
+              child: Row(
+                children: [
+                  const IntrinsicHeight(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Smart",
+                        style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontFamily: 'OpenSans'),
+                      ),
+                      Text(
+                        "Lock",
+                        style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontFamily: 'OpenSans'),
+                      ),
+                      Text(
+                        "Door",
+                        style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontFamily: 'OpenSans'),
+                      ),
+                      Text(
+                        "Safe your Home",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      )
+                    ],
+                  )),
+                  IntrinsicHeight(
+                      child: Padding(
+                    padding: EdgeInsets.only(top: 60),
+                    child: Image.asset('assets/images/door.png', height: 250),
+                  )),
+                ],
+              ))),
+    ));
   }
 }
